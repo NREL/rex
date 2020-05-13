c62958602a62581c2ca9406e192c8420266ae53b
Michael Rossol <mrossol@gmail.com>
2020-05-13 11:39:42 -0600

    typo in col_map

--- rex/resource_extraction.py
+++ rex/resource_extraction.py
@@ -161,7 +161,7 @@ class ResourceX(Resource):
             if c == 'timezone':
                 col_map[c] = 'Time Zone'
             elif c == 'gid':
-                col_map == 'Location ID'
+                col_map = 'Location ID'
             else:
                 col_map[c] = c.capitalize()
 
