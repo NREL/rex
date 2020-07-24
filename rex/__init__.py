3cefb51cd520662524cc2eb23a817c1bacb9a9cb
Michael Rossol <mrossol@gmail.com>
2020-07-24 14:11:57 -0600

    update relative imports

--- rex/__init__.py
+++ rex/__init__.py
@@ -14,7 +14,7 @@ from rex.resource import Resource
 from rex.resource_extraction import (ResourceX, MultiYearResourceX,
                                      NSRDBX, MultiFileNSRDBX, MultiYearNSRDBX,
                                      WindX, MultiFileWindX, MultiYearWindX)
-from rex.utilities import init_logger, init_mult, SpawnProcessPool
+from rex.utilities import init_logger, init_mult, SpawnProcessPool, Retry
 
 from rex.version import __version__
 
