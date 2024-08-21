29d531c17c3cfefd86c8b64d5e248d4e258b8825
Paul Pinchuk <ppinchuk@nrel.gov>
2024-08-21 09:52:25 -0600

    Add a test that uses other wildcards

--- tests/test_multi_time_resource.py
+++ tests/test_multi_time_resource.py
@@ -46,6 +46,16 @@ def MultiTimeNSRDB_wildcard_list_res():
 
 
 @pytest.fixture
+def MultiTimeNSRDB_pattern_list_res():
+    """
+    Init NSRDB resource handler
+    """
+    files = [os.path.join(TESTDATADIR, 'nsrdb/ri_100_nsrdb_20?[2,3].h5')]
+
+    return MultiTimeNSRDB(files)
+
+
+@pytest.fixture
 def MultiTimeWind_res():
     """
     Init WindResource resource handler
@@ -309,6 +319,50 @@ class TestMultiTimeWildcardList:
         check_properties(MultiTimeNSRDB_wildcard_list_res, ds_name)
         assert len(MultiTimeNSRDB_wildcard_list_res.h5.files) >= 2
         MultiTimeNSRDB_wildcard_list_res.close()
+
+
+class TestMultiTimePatternList:
+    """
+    Test multi time resource handler from list of files with patterns
+    """
+    @staticmethod
+    def test_res(MultiTimeNSRDB_pattern_list_res):
+        """
+        test NSRDB class calls
+        """
+        check_res(MultiTimeNSRDB_pattern_list_res)
+        assert len(MultiTimeNSRDB_pattern_list_res.h5.files) >= 2
+        MultiTimeNSRDB_pattern_list_res.close()
+
+    @staticmethod
+    def test_meta(MultiTimeNSRDB_pattern_list_res):
+        """
+        test extraction of NSRDB meta data
+        """
+        check_meta(MultiTimeNSRDB_pattern_list_res)
+        assert len(MultiTimeNSRDB_pattern_list_res.h5.files) >= 2
+        MultiTimeNSRDB_pattern_list_res.close()
+
+    @staticmethod
+    def test_time_index(MultiTimeNSRDB_pattern_list_res):
+        """
+        test extraction of NSRDB time_index
+        """
+        check_time_index(MultiTimeNSRDB_pattern_list_res)
+        assert len(MultiTimeNSRDB_pattern_list_res.h5.files) >= 2
+        MultiTimeNSRDB_pattern_list_res.close()
+
+    @staticmethod
+    def test_ds(MultiTimeNSRDB_pattern_list_res, ds_name='dni'):
+        """
+        test extraction of a variable array, attributes, and properties
+        """
+        check_dset(MultiTimeNSRDB_pattern_list_res, ds_name)
+        check_attrs(MultiTimeNSRDB_pattern_list_res, ds_name)
+        check_properties(MultiTimeNSRDB_pattern_list_res, ds_name)
+        assert len(MultiTimeNSRDB_pattern_list_res.h5.files) >= 2
+        MultiTimeNSRDB_pattern_list_res.close()
+
 
 
 class TestMultiTimeWindResource:
