1ccd031f6a1f8fd2e27ca6a2897ccca7e37bf617
ppinchuk <pinchukpaul@gmail.com>
2025-02-03 00:12:43 -0700

    [WIP] Crude first pass of backend implementation

---
+++ rex/external/rexarray.py
@@ -0,0 +1,492 @@
+# -*- coding: utf-8 -*-
+"""
+rex backend for xarray implementation
+
+Heavily based on the h5netcdf backend:
+https://github.com/pydata/xarray/blob/main/xarray/backends/h5netcdf_.py
+"""
+import io
+import os
+import json
+from pathlib import Path
+
+import h5py
+import numpy as np
+import dask.array as da
+
+from xarray import coding
+from xarray.backends.common import (
+    AbstractDataStore,
+    BackendArray,
+    BackendEntrypoint,
+    _normalize_path,
+    _open_remote_file,
+    find_root_and_group,
+)
+from xarray.backends.file_manager import CachingFileManager, DummyFileManager
+from xarray.backends.locks import (HDF5_LOCK, combine_locks, ensure_lock,
+                                   get_write_lock)
+from xarray.backends.store import StoreBackendEntrypoint
+from xarray.core import indexing
+from xarray.core.utils import (
+    FrozenDict,
+    emit_user_level_warning,
+    is_remote_uri,
+    read_magic_number_from_file,
+    try_read_magic_number_from_file_or_path,
+    close_on_error,
+)
+from xarray.core.variable import Variable
+
+
+TI_DTYPE = np.dtype('datetime64[ns]')
+_SA_FN = Path(__file__).parent / "standard_attrs.json"
+with _SA_FN.open(encoding="utf-8") as fh:
+    _SA = json.load(fh)
+_EN_FN = Path(__file__).parent / "encondings.json"
+with _EN_FN.open(encoding="utf-8") as fh:
+    _EN = json.load(fh)
+
+
+def _get_h5_fn(handle):
+    try:
+        return handle.filename
+    except AttributeError:
+        return _get_h5_fn(handle.file)
+
+
+def _h5_root_or_group(handle, group, mode):
+    if group in {None, "", "/"}:
+        return handle
+
+    if not isinstance(group, str):
+        raise ValueError("group must be a string or None")
+
+    path = group.strip("/").split("/")
+    for key in path:
+        try:
+            handle = handle[key]
+        except KeyError as e:
+            if mode != "r":
+                handle = handle.create_group(key)
+            else:
+                # wrap error to provide slightly more helpful message
+                raise OSError(f"group not found: {key}", e) from e
+    return handle
+
+
+def _rex_var_dtype(variable_name, in_dtype):
+    if _is_time_index(variable_name):
+        return TI_DTYPE
+
+    if in_dtype is str:
+        # use object dtype (with additional vlen string metadata)
+        # because that's the only way in numpy to represent variable
+        # length strings and to check vlen string dtype in further steps
+        # it also prevents automatic string concatenation via
+        # conventions.decode_cf_variable
+        in_dtype = coding.strings.create_vlen_dtype(str)
+
+    return in_dtype
+
+def _read_attributes(h5_var):
+    # GH451
+    # to ensure conventions decoding works properly on Python 3,
+    # decode all bytes attributes to strings
+    attrs = {}
+    for k, v in h5_var.attrs.items():
+        if k not in ["_FillValue", "missing_value"]:
+            if isinstance(v, bytes):
+                try:
+                    v = v.decode("utf-8")
+                except UnicodeDecodeError:
+                    emit_user_level_warning(
+                        f"'utf-8' codec can't decode bytes for attribute "
+                        f"{k!r} of h5 object {h5_var.name!r}, "
+                        f"returning bytes undecoded.",
+                        UnicodeWarning,
+                    )
+        attrs[k] = v
+    return attrs
+
+
+def _detect_dimensions(name, var, meta_index):
+    if _is_from_meta(meta_index):
+        return ["gid"]
+
+    if _is_time_index(name):
+        return ["time_index"]
+
+    return ["time_index", "gid"][:len(var.shape)]
+
+
+def _compile_attrs(name, var, meta_index):
+    attrs = {}
+    for stand_name, stand_attrs in _SA.items():
+        if name.startswith(stand_name):
+            attrs.update(stand_attrs)
+
+    attrs.update(_read_attributes(var))
+    if _is_from_meta(meta_index):
+        attrs.setdefault("description",
+                            "Extracted from H5 file 'meta' variable")
+
+    return attrs
+
+
+def _compile_encoding(name, var, fn, dimensions, orig_shape):
+    # netCDF4 specific encoding
+    encoding = {
+        "chunksizes": var.chunks,
+        "fletcher32": var.fletcher32,
+        "shuffle": var.shuffle,
+    }
+    for encoding_name, default_encoding in _EN.items():
+        if name.startswith(encoding_name):
+            encoding.update(default_encoding)
+
+    if var.chunks:
+        encoding["preferred_chunks"] = dict(
+            zip(dimensions, var.chunks, strict=True)
+        )
+    # Convert h5py-style compression options to NetCDF4-Python
+    # style, if possible
+    if var.compression == "gzip":
+        encoding["zlib"] = True
+        encoding["complevel"] = var.compression_opts
+    elif var.compression is not None:
+        encoding["compression"] = var.compression
+        encoding["compression_opts"] = var.compression_opts
+
+    # save source so __repr__ can detect if it's local or not
+    encoding["source"] = fn
+    encoding["original_shape"] = orig_shape
+    encoding.setdefault("dtype", var.dtype)
+    return encoding
+
+
+def _is_time_index(variable_name):
+    return variable_name.casefold() == "time_index"
+
+
+def _is_from_meta(idx):
+    return idx > -1
+
+
+class RexMetaVar:
+    def __init__(self, meta_var, dtype):
+        self.chunks = meta_var.chunks
+        self.fletcher32 = meta_var.fletcher32
+        self.shuffle = meta_var.shuffle
+        self.dtype = dtype
+        self.shape = meta_var.shape
+        self.compression = None
+        self.compression_opts = None
+        self.attrs = {}
+
+
+class RexArrayWrapper(BackendArray):
+    __slots__ = ("datastore", "dtype", "shape", "variable_name", "meta_index",
+                 "scale_factor", "adder")
+
+    def __init__(self, variable_name, datastore, dtype, meta_index=-1,
+                 scale_factor=1, adder=0):
+        self.datastore = datastore
+        self.variable_name = variable_name
+        self.meta_index = meta_index
+        self.shape = self.get_array().shape
+        self.dtype = _rex_var_dtype(variable_name, dtype)
+        self.scale_factor = scale_factor
+        self.adder = adder
+
+    def __getitem__(self, key):
+        return indexing.explicit_indexing_adapter(
+            key, self.shape, indexing.IndexingSupport.OUTER_1VECTOR,
+            self._getitem)
+
+    def _getitem(self, key):
+        with self.datastore.lock:
+            array = self.get_array(needs_lock=False)
+            if _is_time_index(self.variable_name):
+                return array[key].astype(TI_DTYPE)
+
+            if _is_from_meta(self.meta_index):
+                if self.variable_name == "gid":
+                    return da.arange(self.shape[0])[key].compute()
+
+                meta_info = array[key]
+                if len(meta_info.shape) < 1:  # scalar index
+                    return np.array([meta_info[self.meta_index]],
+                                    dtype=self.dtype)
+                return np.array([col[self.meta_index] for col in meta_info],
+                                dtype=self.dtype)
+
+            return self._unscale_like_rex(array[key])
+
+    def _unscale_like_rex(self, data):
+        if self.adder == 0:
+            return data / self.scale_factor
+
+        return data * self.scale_factor + self.adder
+
+    def get_array(self, needs_lock=True):
+        ds = self.datastore._acquire(needs_lock)
+        if _is_from_meta(self.meta_index):
+            return ds["meta"]
+        return ds[self.variable_name]
+
+
+class RexStore(AbstractDataStore):
+    """Store for reading NREL-rex style data via h5py"""
+
+    __slots__ = ("_filename", "_group", "_manager", "_mode", "is_remote",
+                 "lock")
+
+    def __init__(self, manager, group=None, mode=None, lock=HDF5_LOCK):
+        if isinstance(manager, h5py.File | h5py.Group):
+            if group is None:
+                root, group = find_root_and_group(manager)
+            else:
+                if type(manager) is not h5py.File:
+                    raise ValueError(
+                        "must supply a h5py.File if the group "
+                        "argument is provided"
+                    )
+                root = manager
+            manager = DummyFileManager(root)
+
+        self._manager = manager
+        self._group = group
+        self._mode = mode
+        self._filename = _get_h5_fn(self.ds)
+        self.lock = ensure_lock(lock)
+
+    @classmethod
+    def open(cls, filename, mode="r", group=None, lock=None, driver=None,
+             driver_kwds=None, storage_options=None):
+        """_summary_
+
+        Parameters
+        ----------
+        filename : _type_
+            _description_
+        mode : str, optional
+            _description_. By default, ``"r"``.
+        group : _type_, optional
+            _description_. By default, ``None``.
+        lock : _type_, optional
+            _description_. By default, ``None``.
+        driver : _type_, optional
+            _description_. By default, ``None``.
+        driver_kwds : _type_, optional
+            _description_. By default, ``None``.
+        storage_options : dict[str, Any] | None, optional
+            _description_. By default, ``None``.
+
+        Returns
+        -------
+        _type_
+            _description_
+
+        Raises
+        ------
+        ValueError
+            _description_
+        ValueError
+            _description_
+        """
+        remote_file = (isinstance(filename, str)
+                       and is_remote_uri(filename)
+                       and driver is None)
+        if remote_file:
+            mode_ = "rb" if mode == "r" else mode
+            filename = _open_remote_file(
+                filename, mode=mode_, storage_options=storage_options
+            )
+
+        if isinstance(filename, bytes):
+            raise ValueError("can't open netCDF4/HDF5 as bytes "
+                             "try passing a path or file-like object")
+        elif isinstance(filename, io.IOBase):
+            magic_number = read_magic_number_from_file(filename)
+            if not magic_number.startswith(b"\211HDF\r\n\032\n"):
+                raise ValueError(f"{magic_number!r} is not the signature "
+                                 "of a valid netCDF4 file")
+
+        kwargs = {"driver": driver}
+        if driver_kwds is not None:
+            kwargs.update(driver_kwds)
+
+        if lock is None:
+            if mode == "r":
+                lock = HDF5_LOCK
+            else:
+                lock = combine_locks([HDF5_LOCK, get_write_lock(filename)])
+
+        manager = CachingFileManager(h5py.File, filename, mode=mode,
+                                     kwargs=kwargs)
+        return cls(manager, group=group, mode=mode, lock=lock)
+
+    def _acquire(self, needs_lock=True):
+        with self._manager.acquire_context(needs_lock) as root:
+            ds = _h5_root_or_group(root, self._group, self._mode)
+        return ds
+
+    @property
+    def ds(self):
+        return self._acquire()
+
+    def open_store_variable(self, name, var, meta_index=-1):
+
+        dimensions = _detect_dimensions(name, var, meta_index)
+        attrs = _compile_attrs(name, var, meta_index)
+        sf = attrs.pop("scale_factor", 1)
+        ao = attrs.pop("add_offset", 0)
+
+        data = indexing.LazilyIndexedArray(RexArrayWrapper(name, self,
+                                                           var.dtype,
+                                                           meta_index,
+                                                           scale_factor=sf,
+                                                           adder=ao))
+
+        encoding = _compile_encoding(name, var, self._filename, dimensions,
+                                     orig_shape=data.shape)
+        return Variable(dimensions, data, attrs, encoding)
+
+    def get_variables(self):
+        return FrozenDict((k, self.open_store_variable(k, v, i))
+                          for k, v, i in self._iter_vars())
+
+    def _iter_vars(self):
+        iter_meta = False
+        for k, v in self.ds.items():
+            if k.casefold() == "coordinates":
+                continue
+            if k == "meta":
+                iter_meta = True
+                continue
+            yield k, v, -1
+
+        if iter_meta:
+            meta_var = self.ds["meta"]
+            yield "gid", RexMetaVar(meta_var, np.dtype("int64")), 0
+            for ind, (name, dtype) in enumerate(meta_var.dtype.fields.items()):
+                yield name, RexMetaVar(meta_var, dtype[0]), ind
+
+    def get_coord_names(self):
+        coords = set()
+        if "time_index" in self.ds:
+            coords.add("time_index")
+
+        if "meta" in self.ds:
+            for name in self.ds["meta"].dtype.fields:
+                coords.add(name)
+            coords.add("gid")
+
+        return coords
+
+    def get_attrs(self):
+        return FrozenDict(_read_attributes(self.ds))
+
+    def get_dimensions(self):
+        return FrozenDict((k, len(v.shape)) for k, v in self.ds.items())
+
+    def get_encoding(self):
+        return FrozenDict()
+
+    def close(self, **kwargs):
+        self._manager.close(**kwargs)
+
+
+class RexBackendEntrypoint(BackendEntrypoint):
+    """
+    Backend for NREL rex-style files based on the h5py package.
+
+    See Also
+    --------
+    backends.RexStore
+    """
+
+    description = (
+        "Open NREL-rex style HDF5 files in Xarray"
+    )
+    url = "https://docs.xarray.dev/en/stable/generated/xarray.backends.H5cfBackendEntrypoint.html"
+    open_dataset_parameters = ["filename_or_obj", "drop_variables", "group",
+                               "lock", "driver", "driver_kwds",
+                               "storage_options"]
+
+    def guess_can_open(self, filename_or_obj):
+        """_summary_
+
+        Parameters
+        ----------
+        filename_or_obj : str | path-like | ReadBuffer | AbstractDataStore
+            _description_
+
+        Returns
+        -------
+        bool
+            _description_
+        """
+        magic_number = try_read_magic_number_from_file_or_path(filename_or_obj)
+        if magic_number is not None:
+            return magic_number.startswith(b"\211HDF\r\n\032\n")
+
+        if isinstance(filename_or_obj, str | os.PathLike):
+            _, ext = os.path.splitext(filename_or_obj)
+            return ext in {".nc", ".nc4", ".cdf"}
+
+        return False
+
+    def open_dataset(self, filename_or_obj, *, drop_variables=None, group=None,
+                     lock=None, driver=None, driver_kwds=None,
+                     storage_options=None):
+        """_summary_
+
+        Parameters
+        ----------
+        filename_or_obj : str | path-like | ReadBuffer | AbstractDataStore
+            _description_
+        drop_variables : str | Iterable[str] | None, optional
+            _description_. By default, ``None``.
+        group : _type_, optional
+            _description_. By default, ``None``.
+        lock : _type_, optional
+            _description_. By default, ``None``.
+        driver : _type_, optional
+            _description_. By default, ``None``.
+        driver_kwds : _type_, optional
+            _description_. By default, ``None``.
+        storage_options : dict[str, Any] | None, optional
+            _description_. By default, ``None``.
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
+        filename_or_obj = _normalize_path(filename_or_obj)
+        store = RexStore.open(filename_or_obj, group=group, lock=lock,
+                              driver=driver, driver_kwds=driver_kwds,
+                              storage_options=storage_options)
+
+        store_entrypoint = StoreBackendEntrypoint()
+        with close_on_error(store):
+            ds = store_entrypoint.open_dataset(store, mask_and_scale=False,
+                                               decode_times=False,
+                                               concat_characters=True,
+                                               decode_coords=False,
+                                               drop_variables=drop_variables,
+                                               use_cftime=False,
+                                               decode_timedelta=False)
+
+            coord_names = (store.get_coord_names()
+                           .intersection(set(ds.variables)))
+            ds = ds.set_coords(coord_names)
+
+            dimension_coords = {name: name for name in {"time_index", "gid"}
+                                if name in coord_names}
+            if dimension_coords:
+                ds = ds.set_index(dimension_coords)
+
+        return ds
