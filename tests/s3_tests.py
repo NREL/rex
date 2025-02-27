370d09541c6ddbb722478fca4709a240305e21af
ppinchuk <pinchukpaul@gmail.com>
2025-02-26 17:04:21 -0700

    Add xarray tests

--- tests/s3_tests.py
+++ tests/s3_tests.py
@@ -7,16 +7,21 @@ separate github action that sets up a local hsds server before running the
 test.
 """
 import numpy as np
+import xarray as xr
 from rex import NSRDB, WindResource, MultiYearResource
 
 
 def test_nsrdb():
     """Test retrieving NSRDB data"""
-    with NSRDB("s3://nrel-pds-nsrdb/current/nsrdb_1998.h5") as res:
+    fp = "s3://nrel-pds-nsrdb/current/nsrdb_1998.h5"
+    with NSRDB(fp) as res:
         dsets = res.dsets
         ghi = res['ghi', 0:10, 0]
         assert isinstance(dsets, list)
         assert isinstance(ghi, np.ndarray)
+
+    with xr.open_dataset(fp, engine="rex") as ds:
+        assert np.allclose(ds["ghi"].isel(time_index=slice(0, 10), gid=0), ghi)
 
 
 def test_wtk():
@@ -27,6 +32,11 @@ def test_wtk():
         ws = res['windspeed_88m', 0:10, 0]
         assert isinstance(dsets, list)
         assert isinstance(ws, np.ndarray)
+
+    with xr.open_dataset(fp, engine="rex") as ds:
+        xr_ws = ds["windspeed_88m"].isel(time_index=slice(0, 10), gid=0)
+        assert np.allclose(xr_ws, ws)
+
 
 
 def test_sup3rcc():
@@ -39,13 +49,23 @@ def test_sup3rcc():
         assert isinstance(dsets, list)
         assert isinstance(temp, np.ndarray)
 
+    with xr.open_dataset(fp, engine="rex") as ds:
+        xr_temp = ds["temperature_2m"].isel(time_index=slice(0, 10), gid=0)
+        assert np.allclose(xr_temp, temp)
+
+
 
 def test_multiyear():
     """Test retrieving multi year NSRDB data"""
-    files = ["s3://nrel-pds-nsrdb/current/nsrdb_199*.h5"]
+    files = "s3://nrel-pds-nsrdb/current/nsrdb_199*.h5"
     with MultiYearResource(files) as res:
         dsets = res.dsets
         ghi = res['ghi', 0:10, 0]
         assert res.shape[0] == 35040  # 2x years at 30min (1998 and 1999)
         assert isinstance(dsets, list)
         assert isinstance(ghi, np.ndarray)
+
+    with xr.open_dataset(files, engine="rex") as ds:
+        xr_ghi = ds["ghi"].isel(time_index=slice(0, 10), gid=0)
+        assert np.allclose(xr_ghi, ghi)
+        assert ds.sizes['time_index'] == 35040
